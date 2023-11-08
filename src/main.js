4dbf3433656867ad5338388f371d96655c2833b8
unknown <you@example.com>
2023-11-08 01:12:49 -0300

    master

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { renderItems } from './view.js';
+import { filterData } from './dataFunctions.js';
+import { sortData } from './dataFunctions.js';
+
+
+import data from './data/dataset.js';
+
+
+
+
+const cartas = document.querySelector("#root"); // llamo al id contenedor tarjetas
+cartas.innerHTML = renderItems(data); // Renderiza dinamicamente la data por tarjeta
+
+//Dom y evento escucha para el filtro campo de estudio
+const selectFiltro = document.querySelector("select[name='filtro']")
+selectFiltro.addEventListener("change",(event)=>{ 
+ const dataFiltrada = filterData(data,"mainField",event.target.value) 
+ cartas.innerHTML = ""; 
+ cartas.innerHTML = renderItems(dataFiltrada);
+});
+
+const ordenado = document.querySelector("select[name='name']");
+ordenado.addEventListener("change",(event)=>{
+ const orden = sortData(data,"name",event.target.value);
+ cartas.innerHTML = "";
+ cartas.innerHTML = renderItems(orden);
+ 
+});
+
+
---
+++ src/view.js
@@ -0,0 +1,26 @@
+export const renderItems = (data) => {
+  let tarjetas = "";
+  data.forEach(element => {
+  tarjetas += `<li class="Info">
+  <dl itemscope itemtype="WomenInTech">
+  <img src="${element.imageUrl}" alt="${element.name}" /> 
+  <dt>Nombre:</dt><dd itemprop="name">"${element.name}"</dd>
+  <dt>Descripción:</dt><dd itemprop="shortDescription">"${element.shortDescription}"</dd>
+  <dt>Año de Nacimiento:</dt><dd itemprop="yearOfBirth">"${element.facts.yearOfBirth}"</dd>
+  <dt>Campo de Estudio:</dt><dd itemprop="mainField">"${element.facts.mainField}"</dd>
+  </dl> </li>`
+    
+  }); 
+
+  return `<ul class="Cards">${tarjetas}</ul>`;
+};
+
+
+
+//export const renderItems = (data) => {
+ //console.log(data)
+  // Aquí comienza tu código y puedes retornar lo que tu necesites
+  //return 'example';
+//};
+
+//function renderItems(data) {
