13d027354b715f1f31180941d5760923ca665908
unknown <you@example.com>
2023-11-21 14:35:27 -0300

    Updates

---
+++ dataFunctions.js
@@ -0,0 +1,35 @@
+//En el archivo dataFunctions.js define las siguientes funciones:
+//una función sortBy que tiene 3 parámetros (data, sortBy, sortOrder) y devuelve el arreglo ordenado
+// una función filterBy que tiene 3 parámetros (data, filterBy, value) y devuelve el arreglo
+
+// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+
+//llamo la data para hacer filtrado
+/*export const mainField = (data) => {
+  let camposEstudio = ["Matemáticas","Filosofía","Ciencia de la Computación","Composición Musical","Física","Actuación","Astronomía","Astrofísica","Química"]
+  let camposFiltrados = camposEstudio.forEach();
+  return camposFiltrados;
+}; */
+
+export function filterData(data, camposEstudio, camposFiltrados) {
+  const tarjetasFiltradas = data.filter((tarjeta) =>
+    tarjeta.facts[camposEstudio].includes(camposFiltrados)
+  );
+  return tarjetasFiltradas;
+}
+//function sortData(data, sortBy, sortOrder)
+export function sortData(data, alfabetico, abecedario) {
+  if (abecedario === "Asc") {
+    return data.sort((a, b) => a[alfabetico].localeCompare(b[alfabetico]));
+  } else if (abecedario === "Desc") {
+    return data.sort((a, b) => b[alfabetico].localeCompare(a[alfabetico]));
+  } else {
+    return data; // Orden
+  }
+}
+
+//function sortData(data, sortBy,sortOrder) {}
+
+//function filterData(data, filterBy, value) {
+//return data.filter(item => item[filterBy] === value);
+//}
---
+++ main.js
@@ -0,0 +1,34 @@
+import { renderItems } from "./view.js";
+import { filterData } from "./dataFunctions.js";
+import { sortData } from "./dataFunctions.js";
+
+import data from "./data/dataset.js";
+
+const cartas = document.querySelector("#root"); // llamo al id contenedor tarjetas
+cartas.innerHTML = renderItems(data); // Renderiza dinamicamente la data por tarjeta
+const dataManipulada = [...data];
+
+//Dom y evento escucha para el filtro campo de estudio
+const selectFiltro = document.querySelector("select[name='filtro']");
+selectFiltro.addEventListener("change", (event) => {
+  const dataFiltrada = filterData(
+    dataManipulada,
+    "mainField",
+    event.target.value
+  );
+  cartas.innerHTML = "";
+  cartas.innerHTML = renderItems(dataFiltrada);
+});
+
+const ordenado = document.querySelector("select[name='name']");
+ordenado.addEventListener("change", (event) => {
+  const orden = sortData(dataManipulada, "name", event.target.value);
+  cartas.innerHTML = "";
+  cartas.innerHTML = renderItems(orden);
+});
+
+const botonBorrar = document.querySelector("button[data-testid='button-clear']");
+botonBorrar.addEventListener("click", function () {
+  cartas.innerHTML = renderItems(data);
+  //cartas.innerHTML = cartas.innerHTML.replace(renderItems(data));
+});
